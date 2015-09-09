0bed9be87c8bda9bb470210c1d97b94c0585b362
Eugene <Creepis2011@gmail.com>
2015-09-10 02:24:06 +0300

    Added Brick form file. Brick ex done!

--- Brick.php
+++ Brick.php
@@ -1,3 +1,12 @@
 <?php
-	
+	function pull_brick($hole, $brick) {
+		sort($hole);
+		sort($brick);
+		if ( ($hole[0] >= $brick[0]) && ($hole[1] >= $brick[1]) ) {
+			echo "Brick passed!";
+		} else {
+			echo "Brick has not passed!";
+		}
+	}
+	pull_brick( array( $_GET["c"], $_GET["d"] ), array( $_GET["a"], $_GET["b"], $_GET["h"] ) );
 ?>
